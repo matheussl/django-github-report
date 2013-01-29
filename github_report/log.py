1a6844699684f6969a54357a287cc0f6a589c81b
Matheus Lima <matheus.se@gmail.com>
2013-01-29 16:48:48 -0300

    add issue generator

--- github_report/log.py
+++ github_report/log.py
@@ -2,8 +2,11 @@ import logging
 import traceback
 
 from django.conf import settings
-from django.core import mail
-from django.views.debug import ExceptionReporter, get_exception_reporter_filter
+from django.views.debug import get_exception_reporter_filter
+
+from github import Github
+
+from .conf import conf
 
 getLogger = logging.getLogger
 logger = getLogger('django.request')
@@ -13,10 +16,10 @@ if not logger.handlers:
 
 
 class GithubIssueHandler(logging.Handler):
-    """An exception log handler that emails log entries to site admins.
+    """An exception log handler that create Github issues.
 
     If the request is passed as the first argument to the log record,
-    request data will be provided in the email report.
+    request data will be provided in Github issue.
     """
 
     def __init__(self):
@@ -25,7 +28,7 @@ class GithubIssueHandler(logging.Handler):
     def emit(self, record):
         try:
             request = record.request
-            subject = '%s (%s IP): %s' % (
+            title = '%s (%s IP): %s' % (
                 record.levelname,
                 (request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS
                  and 'internal' or 'EXTERNAL'),
@@ -34,13 +37,13 @@ class GithubIssueHandler(logging.Handler):
             filter = get_exception_reporter_filter(request)
             request_repr = filter.get_request_repr(request)
         except Exception:
-            subject = '%s: %s' % (
+            title = '%s: %s' % (
                 record.levelname,
                 record.getMessage()
             )
             request = None
             request_repr = "Request repr() unavailable."
-        subject = self.format_subject(subject)
+        title = self.format_title(title)
 
         if record.exc_info:
             exc_info = record.exc_info
@@ -50,18 +53,21 @@ class GithubIssueHandler(logging.Handler):
             stack_trace = 'No stack trace available'
 
         message = "%s\n\n%s" % (stack_trace, request_repr)
-        reporter = ExceptionReporter(request, is_email=True, *exc_info)
-        html_message = self.include_html and reporter.get_traceback_html() or None
-        #mail.mail_admins(subject, message, fail_silently=True, html_message=html_message)
 
-        # TODO: Github() and send an issue to settings.GITHUB_REPOSITORY using GITHUB_TOKEN
+        # create github issue
+
+        github = Github(conf.GITHUB_TOKEN)
+        user = github.get_user(conf.GITHUB_USER)
+        repository = user.get_repo(conf.GITHUB_REPOSITORY_NAME)
+        issue = repository.create_issue(self, title, body=message)
 
 
-    def format_subject(self, subject):
+
+    def format_title(self, title):
         """
         Escape CR and LF characters, and limit length.
-        RFC 2822's hard limit is 998 characters per line. So, minus "Subject: "
-        the actual subject must be no longer than 989 characters.
+        RFC 2822's hard limit is 998 characters per line. So, minus "Title: "
+        the actual title must be no longer than 989 characters.
         """
-        formatted_subject = subject.replace('\n', '\\n').replace('\r', '\\r')
-        return formatted_subject[:989]
+        formatted_title = title.replace('\n', '\\n').replace('\r', '\\r')
+        return formatted_title[:989]
